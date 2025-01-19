f8b1d564902f5b062bfb6c3914f44a073d83c54e
Michal Karzynski <github@karzyn.com>
2025-01-19 20:42:48 +0100

    Add atuin history plugin

---
+++ .xdg.sh
@@ -0,0 +1,7 @@
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.config
+
+export ANSIBLE_HOME="$XDG_DATA_HOME"/ansible
+export TLDR_CACHE_DIR="$XDG_CACHE_HOME"/tldr
+
