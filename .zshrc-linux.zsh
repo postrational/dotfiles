76b002071bf3b3cc93f3798e0a154296785cfa26
Michal K <github@karzyn.com>
2026-01-30 15:54:04 +0100

    Vim for SYSTEMD_EDITOR

--- .zshrc-linux.zsh
+++ .zshrc-linux.zsh
@@ -1,3 +1,6 @@
+export SYSTEMD_EDITOR=vim
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+
 alias ls="ls -ahvlG --color"
 alias open="xdg-open"
 alias sys="sudo systemctl"
@@ -7,5 +10,3 @@ alias apt="sudo apt"
 alias pacman="sudo pacman"
 alias service-status="sudo systemctl list-units --type=service --state=loaded --no-pager"
 alias ufw="sudo ufw"
-
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
