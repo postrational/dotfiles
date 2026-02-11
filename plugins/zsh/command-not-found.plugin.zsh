864bc434bf80a7dc13f65c14e64efd90cded52c0
Michal K <github@karzyn.com>
2026-02-11 10:07:01 +0100

    Update command-not-found handler

--- plugins/zsh/command-not-found.plugin.zsh
+++ plugins/zsh/command-not-found.plugin.zsh
@@ -1,37 +1,37 @@
-# Function to load command-not-found handler
 load_command_not_found_handler() {
-  local file
+  local handler
+  local -a source_handlers=(
+    /usr/share/doc/pkgfile/command-not-found.zsh
+  )
+  local -a exec_handlers=(
+    /usr/lib/command-not-found
+    /usr/share/command-not-found/command-not-found
+  )
 
-  for file in \
-    /usr/share/doc/pkgfile/command-not-found.zsh \
-    "$(brew --repository)/Library/Homebrew/command-not-found/handler.sh" \
-    /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh; do
-    [[ -r "$file" ]] && source "$file" && return 0
-  done
-
-  if [[ -x /usr/lib/command-not-found ]]; then
-    command_not_found_handler() { /usr/lib/command-not-found -- "$1"; return $?; }
-    return 0
-  elif [[ -x /usr/share/command-not-found/command-not-found ]]; then
-    command_not_found_handler() { /usr/share/command-not-found/command-not-found -- "$1"; return $?; }
-    return 0
-  elif [[ -r /usr/share/doc/pkgfile/command-not-found.zsh ]]; then
-    source /usr/share/doc/pkgfile/command-not-found.zsh
-    return 0
+  if (( $+commands[brew] )); then
+    source_handlers+=("$(brew --repository)/Library/Homebrew/command-not-found/handler.sh")
   fi
 
-  # If we get here, no handler was found
+  for handler in "${source_handlers[@]}"; do
+    [[ -r "$handler" ]] && source "$handler" && return 0
+  done
+
+  for handler in "${exec_handlers[@]}"; do
+    if [[ -x "$handler" ]]; then
+      local cmd="$handler"
+      command_not_found_handler() { "$cmd" -- "$1"; return $?; }
+      return 0
+    fi
+  done
+
   command_not_found_handler() {
-    echo "my zsh: command not found: $1" >&2
+    print "zsh: command not found: $1" >&2
     return 127
   }
 
-  print -n $'\e[33m' >&2 # Yellow color
-  print "Warning: No command-not-found handler found." >&2
-  print -n $'\e[0m' >&2 # Reset color
+  print -P "%F{yellow}Warning: No command-not-found handler found.%f" >&2
   return 1
 }
 
-# Load the handler
 load_command_not_found_handler
 unset -f load_command_not_found_handler
